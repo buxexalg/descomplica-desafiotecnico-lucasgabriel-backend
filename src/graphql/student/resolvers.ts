0a0fa7ae6ca4eb19428a36ad9b1aecef43f86539
buxexalg <lucasgralcantara@gmail.com>
2021-07-10 14:32:09 -0300

    feat: add cpf and email validation

--- src/graphql/student/resolvers.ts
+++ src/graphql/student/resolvers.ts
@@ -1,3 +1,7 @@
+import { isCpf } from 'validator-brazil';
+import { validate } from 'email-validator';
+import { UserInputError } from 'apollo-server-express';
+
 import db from '../../database/knex';
 
 interface StudentInterface {
@@ -15,6 +19,18 @@ export const resolvers = {
 			_: StudentInterface,
 			{ name, cpf, email }: StudentInterface
 		): Promise<StudentInterface> {
+			if (!isCpf(cpf)) {
+				throw new UserInputError('Invalid cpf value', {
+					argumentName: 'cpf',
+				});
+			}
+
+			if (!validate(email)) {
+				throw new UserInputError('Invalid email value', {
+					argumentName: 'email',
+				});
+			}
+
 			const student = {
 				name,
 				cpf,
