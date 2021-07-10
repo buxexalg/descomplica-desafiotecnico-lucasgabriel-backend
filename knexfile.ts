a7f5dbe6c39e32ee62f7beaf2aedfdbd7ec66daf
buxexalg <lucasgralcantara@gmail.com>
2021-07-10 16:38:07 -0300

    feat: add knexfile with dotenv

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+import path = require('path');
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+	client: 'pg',
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		ssl: { rejectUnauthorized: false },
+	},
+
+	migrations: {
+		directory: path.join(__dirname, 'src/database/migrations'),
+	},
+	seeds: {
+		directory: path.join(__dirname, 'src/database/seeds'),
+	},
+};
