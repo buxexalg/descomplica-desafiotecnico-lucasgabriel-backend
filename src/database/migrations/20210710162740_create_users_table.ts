7e66140475ddb67f19dac68e1f7602df11c85bad
buxexalg <lucasgralcantara@gmail.com>
2021-07-10 14:11:49 -0300

    feat: add migration file

---
+++ src/database/migrations/20210710162740_create_users_table.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('students', (t) => {
+		t.increments('id');
+		t.string('name').notNullable();
+		t.string('cpf').notNullable().unique();
+		t.string('email').notNullable();
+		t.timestamps(false, true);
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.raw('DROP TABLE students CASCADE');
+}
